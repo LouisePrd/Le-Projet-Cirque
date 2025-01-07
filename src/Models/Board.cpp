2c4413b97830d505351246fe164e141414b69b3a
LouisePrd <louise.peridy22@gmail.com>
2025-01-07 14:48:13 +0100

    ✨ Creating board and update windows

---
+++ src/Models/Board.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "quick_imgui/quick_imgui.hpp"
+#include <imgui.h>
+#include "Board.hpp"
+
+void Board::createTartan() {
+    int rows = 8;
+    int cols = 8;
+
+    std::cout << "Creating board" << std::endl;
+    ImGui::Begin("Board");
+
+
+    ImGui::End();
+}
---
+++ src/Models/Board.hpp
@@ -0,0 +1,11 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+class Board {
+    public:
+        int nbCase;
+        int nbPiece;
+        void createTartan();
+};
+
+#endif
