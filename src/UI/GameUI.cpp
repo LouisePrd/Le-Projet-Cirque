3a1fdc67fb5aa57eab4232180f0de255a99832ae
LouisePrd <louise.peridy22@gmail.com>
2025-01-07 21:46:18 +0100

    💄 Render with good case

--- src/UI/GameUI.cpp
+++ src/UI/GameUI.cpp
@@ -6,19 +6,26 @@
 
 void GameUI::render(Game &game) {
   ImGui::Begin("Game");
-    for(int i = 0; i<game.board.cases.size(); i++) {
-      Case c = game.board.cases[i];
-      if (c.color % 2 == 0)
+  for (int i = 0; i < game.board.cases.size(); i++) {
+    Case c = game.board.cases[i];
+
+    if (c.piece.has_value()) {
+      Piece p = c.piece.value();
+      std::string pieceLabel = p.type;
+      if (p.color == "black")
         ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
       else
         ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
-
-      std::string buttonLabel = std::to_string(c.id);
-      ImGui::Button(buttonLabel.c_str(), ImVec2(50, 50));
-      if (i % 8 != 7)
-        ImGui::SameLine();
       ImGui::PopStyleColor();
 
+      ImGui::Button(pieceLabel.c_str(), ImVec2(50, 50));
+    } else {
+      std::string buttonLabel = "##" + std::to_string(c.id);
+      ImGui::Button(buttonLabel.c_str(), ImVec2(50, 50));
     }
+
+    if (i % 8 != 7)
+      ImGui::SameLine();
+  }
   ImGui::End();
 }
