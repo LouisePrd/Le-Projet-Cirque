0f739346fdb498be86245f865ba40db7c400c1b9
LouisePrd <louise.peridy22@gmail.com>
2025-01-07 20:35:00 +0100

    ✨ Clean board display

--- src/UI/GameUI.cpp
+++ src/UI/GameUI.cpp
@@ -6,6 +6,19 @@
 
 void GameUI::render(Game &game) {
   ImGui::Begin("Game");
-  ImGui::Text("Game is running");
+    for(int i = 0; i<game.board.cases.size(); i++) {
+      Case c = game.board.cases[i];
+      if (c.color % 2 == 0)
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
+      else
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+
+      std::string buttonLabel = std::to_string(c.id);
+      ImGui::Button(buttonLabel.c_str(), ImVec2(50, 50));
+      if (i % 8 != 7)
+        ImGui::SameLine();
+      ImGui::PopStyleColor();
+
+    }
   ImGui::End();
 }
