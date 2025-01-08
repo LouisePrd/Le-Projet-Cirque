01d5364882697064c3dead94c995a7d31abc7f88
LouisePrd <louise.peridy22@gmail.com>
2025-01-08 11:20:15 +0100

    🔒️ Secured attributes Piece

--- src/Models/Piece.cpp
+++ src/Models/Piece.cpp
@@ -4,7 +4,6 @@
 
 Piece::Piece() {
   id = 0;
-  type = "";
   color = "";
   x = 0;
   y = 0;
@@ -12,12 +11,23 @@ Piece::Piece() {
   idPlayer = 0;
 }
 
-Piece::Piece(int id, std::string type, std::string color, int x, int y, bool selected, int idPlayer) {
+Piece::Piece(int id, std::string color, int x, int y, bool selected, int idPlayer) {
   this->id = id;
-  this->type = type;
   this->color = color;
   this->x = x;
   this->y = y;
   this->selected = selected;
   this->idPlayer = idPlayer;
 }
+
+int Piece::getId() const { return id; }
+std::string Piece::getColor() const { return color; }
+int Piece::getX() const { return x; }
+int Piece::getY() const { return y; }
+bool Piece::isSelected() const { return selected; }
+int Piece::getIdPlayer() const { return idPlayer; }
+
+void Piece::setId(int id) { this->id = id; }
+void Piece::setX(int x) { this->x = x; }
+void Piece::setY(int y) { this->y = y; }
+void Piece::setSelected(bool selected) { this->selected = selected; }
--- src/Models/Piece.hpp
+++ src/Models/Piece.hpp
@@ -4,17 +4,31 @@
 #include <string>
 
 class Piece {
-    public:
+    private:
         int id;
-        std::string type;
         std::string color;
         int x;
         int y;
         bool selected;
         int idPlayer;
-
+    
+    public:
     Piece();
-    Piece(int id, std::string type, std::string color, int x, int y, bool selected, int idPlayer);
+    Piece(int id, std::string color, int x, int y, bool selected, int idPlayer);
+
+    // Getters
+    int getId() const;
+    std::string getColor() const;
+    int getX() const;
+    int getY() const;
+    bool isSelected() const;
+    int getIdPlayer() const;
+
+    // Setters
+    void setId(int id);
+    void setX(int x);
+    void setY(int y);
+    void setSelected(bool selected);
 };
 
 #endif
