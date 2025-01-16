c4695ab27a2363f8b60022923b745396809a2757
LouisePrd <louise.peridy22@gmail.com>
2025-01-16 13:26:29 +0100

    🎨 Folder Models into Gameplay

---
+++ src/Gameplay/Piece.hpp
@@ -0,0 +1,40 @@
+#ifndef PIECE_HPP
+#define PIECE_HPP
+
+#include <string>
+
+class Piece {
+protected:
+  int id;
+  std::string color;
+  int x;
+  int y;
+  bool selected;
+  int idPlayer;
+  std::string type;
+
+public:
+  Piece();
+  Piece(int id, std::string type, std::string color, int x, int y, bool selected, int idPlayer);
+  virtual bool isMoveValid(std::pair<int, int> move);
+
+  // Getters
+  int getId() const;
+  std::string getColor() const;
+  int getX() const;
+  int getY() const;
+  bool isSelected() const;
+  int getIdPlayer() const;
+  std::string getType() const;
+
+  // Setters
+  void setId(int id);
+  void setX(int x);
+  void setY(int y);
+  void setSelected(bool selected);
+  void setIdPlayer(int idPlayer);
+  void setColor(std::string color);
+  void setType(std::string type);
+};
+
+#endif
